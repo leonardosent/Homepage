c5e445cdc4c2c9e43cae4f5725e4b49b81cc402b
leonardosent <leonardosent@gmail.com>
2023-06-04 20:43:30 -0700

    Updated index.html, styles.css, and index.js

--- assets/index.js
+++ assets/index.js
@@ -8,19 +8,19 @@ if (typeof window !== 'undefined') {
         console.log('Localstorage Check Complete')
     }
 
-    const lightModeButton = document.getElementById("lightModeButton")
+    // const lightModeButton = document.getElementById("lightModeButton")
 
-    lightModeButton.onClick = function() {
-        if (lightModeButton.className == "btn btn-dark") {
-            lightModeButton.innerHtml = "🌑"
-            lightModeButton.className = "btn btn-light"
-            if (localStorageAllowed == true) { localStorage.setItem("lightMode", "dark") }
-        } else {
-            lightModeButton.innerHtml = "☀️"
-            lightModeButton.className = "btn btn-dark"
-            if (localStorageAllowed == true) { localStorage.setItem("lightMode", "light") }
-        }
-    };
+    // lightModeButton.onClick = function() {
+    //     if (lightModeButton.className == "btn btn-dark") {
+    //         lightModeButton.innerHtml = "🌑"
+    //         lightModeButton.className = "btn btn-light"
+    //         if (localStorageAllowed == true) { localStorage.setItem("lightMode", "dark") }
+    //     } else {
+    //         lightModeButton.innerHtml = "☀️"
+    //         lightModeButton.className = "btn btn-dark"
+    //         if (localStorageAllowed == true) { localStorage.setItem("lightMode", "light") }
+    //     }
+    // };
 
 } else {
     console.log('JS Running on Server')
